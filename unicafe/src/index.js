896fa0d287ec357c4127b21d0047319d79947467
Kiti Suupohja <kitsu@kapsi.fi>
2021-01-10 22:21:33 +0200

    Create table from results. Move buttons and resultlines to their own components

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -5,14 +5,14 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  let all = good+neutral+bad;
+  let all = good+neutral+bad
 
   return (
     <div>
       <h1>Give feedback</h1>
-      <button id="good" onClick={() => setGood(good+1)}>good</button>
-      <button id="neutral" onClick={() => setNeutral(neutral+1)}>neutral</button>
-      <button id="bad" onClick={() => setBad(bad+1)}>bad</button>
+      <Button text="good" emotion={good} action={setGood}/>
+      <Button text="neutral" emotion={neutral} action={setNeutral}/>
+      <Button text="bad" emotion={bad} action={setBad}/>
 
       <h1>Statistics</h1>
       <Statistics good={good} bad={bad} neutral={neutral} all={all}/>
@@ -20,23 +20,44 @@ const App = () => {
   )
 }
 
+const Button = (props) => {
+  return(
+    <button onClick={() => props.action(props.emotion+1)}>{props.text}</button>
+  )
+}
+
 const Statistics = (props) => {
+  let avg = CalcAvg(props.good, props.bad, props.all)
+  let pos = CalcPos(props.good, props.all) + " %"
+
   if (parseInt(props.all) > 0) {
     return (
-      <>
-      <p>Good {props.good}</p>
-      <p>Neutral {props.neutral}</p>
-      <p>Bad {props.bad}</p>
-      <p>All {props.all}</p>
-      <p>Average {CalcAvg(props.good, props.bad, props.all)} </p>
-      <p>Positive {CalcPos(props.good, props.all)} % </p>
-      </>
+      <table>
+        <tbody>
+        <StatisticsLine feedback="Good" amount={props.good}/>
+        <StatisticsLine feedback="Neutral" amount={props.neutral}/>
+        <StatisticsLine feedback="Bad" amount={props.bad}/>
+        <StatisticsLine feedback="All" amount={props.all}/>
+        <StatisticsLine feedback="Average" amount={avg}/>
+        <StatisticsLine feedback="Positive" amount={pos}/>
+      </tbody>
+      </table>
     )
   } else {
     return (
       <p>No feedback given</p>
     )
   }  
+}
+
+const StatisticsLine = (props) => {
+  return(
+    <tr>
+      <td>{props.feedback}</td>
+      <td>{props.amount}</td>
+    </tr>
+  )
+  
 }
 
 const CalcAvg = (good, bad, all) => {
